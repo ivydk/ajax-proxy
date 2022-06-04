f424bfc274644c1af6b76466cd69bff6f956ca97
Ivy Dekker <ivy_dk@hotmail.com>
2022-06-04 21:51:46 +0200

    change the DOM

--- proxy-without-authorization/assets/app.js
+++ proxy-without-authorization/assets/app.js
@@ -8,8 +8,20 @@ function displayData(){
   fetch("../controller.php").then(function(response){
     return response.json();
   }).then(function(data){
+    // Changes DOM elements
+    document.querySelector('#title').innerHTML = data.collection.name;
+    document.getElementById('subtitle').innerHTML = data.collection.description;
+    display_image('banner', data.collection.banner_image_url);
+    display_image('featured_image_url', data.collection.featured_image_url);
+
+    // Debug
     console.log(data);
+
   }).catch(function(error){
     console.log(error);
   })
+}
+
+function display_image($id, $img_url){
+  document.getElementById($id).src = $img_url
 }
