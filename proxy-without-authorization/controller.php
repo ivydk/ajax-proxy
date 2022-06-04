8621bcb8324b7a15ee188308c41dafd5c64586c3
Ivy Dekker <ivy_dk@hotmail.com>
2022-06-04 21:52:15 +0200

    changed api

--- proxy-without-authorization/controller.php
+++ proxy-without-authorization/controller.php
@@ -2,9 +2,8 @@
 include("./proxy.php");
 
 //setup the Proxy with the right credentials
-$mySwapiProxy = new Proxy('https://swapi.dev/api');
+$mySwapiProxy = new Proxy('https://api.opensea.io/api/v1/');
 //get the data
-$result = $mySwapiProxy->getData('people/1');
-
+$result = $mySwapiProxy->getData('collection/doodles-official');
 //print the data to the frontend.
 print_r($result);
